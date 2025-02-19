36b9a51944d14cc34951cbcd1f157a94d335900d
Nader Aldweik <n.aldweik@eon.local>
2025-02-19 11:56:03 +0300

    Hello Nader

---
+++ Task13.js
@@ -0,0 +1,19 @@
+
+
+
+function forMe(arr){
+
+    var arr = [];
+
+
+    for (arr = 1 ; arr.length -1 < 5; arr ++){
+
+
+        console.log("Hello nader")
+    
+    
+    }
+
+    return arr;
+}
+ console.log (forMe (11, 13 ,12))
